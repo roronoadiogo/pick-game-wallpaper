10afe5dc7959b9834ed8568f0a41ec5f06a10023
Vinicius D. Alves <roronoadiogo@gmail.com>
2023-11-07 21:53:13 -0300

    feat: adding the first csv model from tasklist

--- app/utils/utils.go
+++ app/utils/utils.go
@@ -2,18 +2,41 @@ package utils
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 )
 
 func FindProcessGame() {
 
-	cmd := exec.Command("TASKLIST", "/V /NH /FI")
+	cmd := exec.Command("TASKLIST", "/V", "/FO", "CSV")
 	result, err := cmd.Output()
 
 	if err != nil {
-		fmt.Println("Error not found any process")
+		fmt.Println("Error not found any process", err)
 	}
 
+	exportCsvProcess(result)
+
 	fmt.Println(result)
+
+}
+
+func exportCsvProcess(taskCsv []byte) (*os.File, error) {
+
+	filename := "data_test.csv"
+	file, err := os.Create(filename)
+
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+	}
+
+	defer file.Close()
+
+	_, err = file.Write(taskCsv)
+	if err != nil {
+		fmt.Println("Error writing to the file:", err)
+	}
+
+	return file, nil
 
 }
